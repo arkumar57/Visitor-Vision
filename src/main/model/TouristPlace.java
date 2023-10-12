afbf8f38cd3d1b9dede8fd47610e8069a657d0ea
arun <akumar57@student.ubc.ca>
2023-10-12 16:31:18 -0700

    Phase1Again1

--- src/main/model/TouristPlace.java
+++ src/main/model/TouristPlace.java
@@ -5,10 +5,6 @@ public class TouristPlace {
     private String city;
     private String country;
     private String location;
-    private String typeofPlace;
-    private String speciality;
-    private String guidelines;
-    private String writtenReview;
     private int rating;
     private int price;
 
@@ -27,73 +23,26 @@ public class TouristPlace {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
 
     public String getCity() {
         return city;
     }
 
-    public void setCity(String city) {
-        this.city = city;
-    }
 
     public String getCountry() {
         return country;
     }
 
-    public void setCountry(String country) {
-        this.country = country;
-    }
 
     public String getLocation() {
         return location;
     }
 
-    public void setLocation(String location) {
-        this.location = location;
-    }
-
-    public String getTypeofPlace() {
-        return typeofPlace;
-    }
-
-    public void setTypeofPlace(String typeofPlace) {
-        this.typeofPlace = typeofPlace;
-    }
-
-    public String getSpeciality() {
-        return speciality;
-    }
-
-    public void setSpeciality(String speciality) {
-        this.speciality = speciality;
-    }
-
-    public String getGuidelines() {
-        return guidelines;
-    }
-
-    public void setGuidelines(String guidelines) {
-        this.guidelines = guidelines;
-    }
 
     public int getRating() {
         return rating;
     }
 
-    public void setRating(int rating) {
-        this.rating = rating;
-    }
-
-    public String getWrittenReview() {
-        return writtenReview;
-    }
-
-    public void setWrittenReview(String writtenReview) {
-        this.writtenReview = writtenReview;
-    }
 
     public int getPrice() {
         return price;
